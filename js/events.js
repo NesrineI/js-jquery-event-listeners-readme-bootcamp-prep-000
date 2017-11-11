cd81d19509e0dc2b2b6e7ffd15451d80b5bfaf54
Nesrine <nesrinakon@gmail.com>
2017-11-11 20:56:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,3 +10,13 @@ $(document).ready(function(){
      alert("Hey!");
    })
  }
+ function frameIt() {
+   $('img').on('load', function(){
+     $(this).addClass('tasty');
+   })
+ }
+ function submitIt(){
+   $('form').on('submit', function(){
+     alert("Your form is going to be submitted now.");
+   })
+ }
